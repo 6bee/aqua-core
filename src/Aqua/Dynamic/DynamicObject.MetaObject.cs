991144c46de35dc04162b2aff7686c12a8a1bf2e
Christof Senn <christof.senn@gmail.com>
2016-03-24 00:46:56 +0100

    CoreClr and WinRT

--- src/Aqua/Dynamic/DynamicObject.MetaObject.cs
+++ src/Aqua/Dynamic/DynamicObject.MetaObject.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
 
-#if !NET35 && !CORECLR
+#if !(CORECLR || WINRT || NET35)
 
 namespace Aqua.Dynamic
 {
