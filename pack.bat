258e2b557f89bce407b7784a8b8f7fa50de9c923
Christof Senn <christof.senn@gmail.com>
2016-04-13 23:50:36 +0200

    temporary solution for build / test / pack (using dnu, dnx, and dotnet-cli)

---
+++ pack.bat
@@ -0,0 +1,1 @@
+dnu restore && dnu build src\Aqua test\Aqua.NET35.Tests test\Aqua.Tests && dnx -p test\Aqua.Tests test && dotnet pack src\Aqua --output artifacts --configuration Release --version-suffix 002
