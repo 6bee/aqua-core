20bb5e8170e387cd4eb98103ee3162eaee84fa51
Christof Senn <christof.senn@gmail.com>
2016-08-13 01:03:57 +0200

    4.0.0-alpha-029

--- pack.bat
+++ pack.bat
@@ -1,1 +1,2 @@
-dnu restore && dnu build src\Aqua test\Aqua.NET35.Tests test\Aqua.Tests && dnx -p test\Aqua.Tests test && dotnet pack src\Aqua --output artifacts --configuration Debug --version-suffix 012
+dotnet restore && dotnet build src\Aqua test\Aqua.Tests && dotnet test test\Aqua.Tests && dotnet pack src\Aqua --output artifacts --configuration Release --version-suffix 029
+pause
