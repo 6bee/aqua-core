7835e43336ec1043b76bcf6ebeeac828e59737ce
Christof Senn <christof.senn@gmail.com>
2017-01-09 23:35:55 +0100

    4.0.0-alpha-031

--- pack.bat
+++ pack.bat
@@ -1,1 +1,1 @@
-dotnet restore && dotnet build src\Aqua test\Aqua.Tests && dotnet test test\Aqua.Tests && dotnet pack src\Aqua --output artifacts --configuration Release --version-suffix 030
+dotnet restore && dotnet build src\Aqua test\Aqua.Tests && dotnet test test\Aqua.Tests && dotnet pack src\Aqua --output artifacts --configuration Release --version-suffix 031
