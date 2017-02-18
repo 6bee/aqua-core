241723f4340c9cac341658363144c3a811bd1baa
Christof Senn <christof.senn@gmail.com>
2017-02-18 04:45:28 +0100

    4.0.0-alpha-038

--- pack.bat
+++ pack.bat
@@ -1,1 +1,1 @@
-dotnet restore && dotnet build src\Aqua test\Aqua.Tests.TestObjects1 test\Aqua.Tests.TestObjects2 test\Aqua.Tests && dotnet test test\Aqua.Tests && dotnet pack src\Aqua --output artifacts --configuration Debug --version-suffix 037
+clean && dotnet restore && dotnet build src\Aqua test\Aqua.Tests.TestObjects1 test\Aqua.Tests.TestObjects2 test\Aqua.Tests && dotnet test test\Aqua.Tests && dotnet pack src\Aqua --output artifacts --configuration Debug --version-suffix 038
