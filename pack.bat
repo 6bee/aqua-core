a265292dff6e0a913e4e175d0570982399a1c5f2
Christof Senn <christof.senn@gmail.com>
2017-06-15 23:01:10 +0200

    release 4.0.1 (build in release mode!)

--- pack.bat
+++ pack.bat
@@ -1,11 +1,13 @@
 @echo off
+set configuration=Release
+set version-suffix=""
 clean ^
   && dotnet restore ^
-  && dotnet build src\Aqua ^
-  && dotnet build src\Aqua.Newtonsoft.Json ^
-  && dotnet build test\Aqua.Tests.TestObjects1 ^
-  && dotnet build test\Aqua.Tests.TestObjects2 ^
-  && dotnet build test\Aqua.Tests ^
-  && dotnet test test\Aqua.Tests\Aqua.Tests.csproj ^
-  && dotnet pack src\Aqua\Aqua.csproj --output "..\..\artifacts" --configuration Debug --include-symbols --version-suffix "" ^
-  && dotnet pack src\Aqua.Newtonsoft.Json\Aqua.Newtonsoft.Json.csproj --output "..\..\artifacts" --configuration Debug --include-symbols --version-suffix ""
+  && dotnet build src\Aqua --configuration %configuration% ^
+  && dotnet build src\Aqua.Newtonsoft.Json --configuration %configuration% ^
+  && dotnet build test\Aqua.Tests.TestObjects1 --configuration %configuration% ^
+  && dotnet build test\Aqua.Tests.TestObjects2 --configuration %configuration% ^
+  && dotnet build test\Aqua.Tests --configuration %configuration% ^
+  && dotnet test test\Aqua.Tests\Aqua.Tests.csproj --configuration %configuration% ^
+  && dotnet pack src\Aqua\Aqua.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix% ^
+  && dotnet pack src\Aqua.Newtonsoft.Json\Aqua.Newtonsoft.Json.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix %version-suffix%
