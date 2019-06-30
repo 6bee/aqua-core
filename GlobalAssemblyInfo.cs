6e48a6e63643c4258ec5bdcebcfc4c415707b5aa
Christof Senn <christof.senn@gmail.com>
2019-06-30 06:44:29 +0200

    consolidated project configurations

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -3,16 +3,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion("4.0.0.0")]
