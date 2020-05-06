107e2d291e59802558d9f5e50e4af8f888a476b6
Christof Senn <christof.senn@gmail.com>
2020-05-06 15:23:29 +0200

    added method's return type to be able to match cast operator accordingly - resolve issue #30

---
+++ test/Aqua.Tests/TypeSystem/TypeResolver/When_resolving_explicit_cast_operator.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+namespace Aqua.Tests.TypeSystem.TypeResolver
+{
+    using Aqua.TypeSystem;
+    using Shouldly;
+    using System;
+    using System.Linq.Expressions;
+    using Xunit;
+
+    public class When_resolving_explicit_cast_operator
+    {
+        [Fact]
+        public void ResolveMethod_ExplicitCast_should_return_original_method_info()
+        {
+            var typeResolver = new TypeResolver();
+
+            Expression<Func<decimal?, double?>> expr = x => (double?)x;
+            System.Reflection.MethodInfo methodInfo = ((UnaryExpression)expr.Body).Method;
+            Aqua.TypeSystem.MethodInfo mappedMethod = new Aqua.TypeSystem.MethodInfo(methodInfo);
+            System.Reflection.MethodInfo resolvedMethod = mappedMethod.ResolveMethod(typeResolver);
+
+            resolvedMethod.ShouldBe(methodInfo);
+        }
+    }
+}
