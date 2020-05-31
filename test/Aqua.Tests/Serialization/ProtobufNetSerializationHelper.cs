92c5cc1b676f50af086f0bc10d499dc5f7e7d69d
Christof Senn <christof.senn@gmail.com>
2020-05-31 15:16:57 +0200

    temp/wip: exclude enums from protobuf-tests

--- test/Aqua.Tests/Serialization/ProtobufNetSerializationHelper.cs
+++ test/Aqua.Tests/Serialization/ProtobufNetSerializationHelper.cs
@@ -2,6 +2,7 @@
 
 namespace Aqua.Tests.Serialization
 {
+    using Aqua.TypeSystem;
     using System;
     using System.Collections;
     using System.Linq;
@@ -21,6 +22,7 @@ namespace Aqua.Tests.Serialization
             Skip.If(type.Is<DateTimeOffset>(), "Data type not supported by out-of-the-box protobuf-net");
             Skip.If(type.Is<BigInteger>(), "Data type not supported by out-of-the-box protobuf-net");
             Skip.If(type.Is<Complex>(), "Data type not supported by out-of-the-box protobuf-net");
+            Skip.If(type.IsEnum(), "Data type not supported by out-of-the-box protobuf-net");
 
             // TODO: check protobuf-net doc
             Skip.If(type.IsCollection() && ((IEnumerable)value).Cast<object>().All(x => x is null), "Collections with only null elements are set null by out-of-the-box protobuf-net");
