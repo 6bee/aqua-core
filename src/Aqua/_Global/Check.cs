168da7b62fec351018ea19f96b5c54c5ba6e4c5d
Christof Senn <christof.senn@gmail.com>
2020-06-07 23:29:41 +0200

    moved Check to global namespance

---
+++ src/Aqua/_Global/Check.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+[SuppressMessage(
+    "Major Bug",
+    "S3903:Types should be defined in named namespaces",
+    Justification = "Global extension method with internal visibility. Support friend assemblies in different namespances.")]
+internal static class Check
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public sealed class ValidatedNotNullAttribute : Attribute
+    {
+    }
+
+    public static T CheckNotNull<T>([ValidatedNotNull] this T value, string name) => value ?? throw new ArgumentNullException(name);
+}
