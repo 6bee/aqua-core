58056145c903c7cb43218ca16ec9fae55b3302e1
Christof Senn <christof.senn@gmail.com>
2020-06-21 15:34:40 +0200

    `AggressiveInlining`

--- src/Aqua/_Global/Check.cs
+++ src/Aqua/_Global/Check.cs
@@ -2,7 +2,9 @@
 
 using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 [EditorBrowsable(EditorBrowsableState.Never)]
 [SuppressMessage(
@@ -11,5 +13,7 @@ using System.Diagnostics.CodeAnalysis;
     Justification = "Global extension method with internal visibility. Support friend assemblies in different namespances.")]
 internal static class Check
 {
+    [DebuggerStepThrough]
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static T CheckNotNull<T>([ValidatedNotNull] this T value, string name) => value ?? throw new ArgumentNullException(name);
 }
