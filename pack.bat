b6b4243e990d132c0218e8005c08ca595a929b5f
Christof Senn <christof.senn@gmail.com>
2020-12-29 18:42:53 +0100

    include git commit hash in assembly and package info

--- pack.bat
+++ pack.bat
@@ -3,6 +3,6 @@ set configuration=Debug
 clean ^
   && dotnet test test\Aqua.Tests          --configuration %configuration% ^
   && dotnet test test\Aqua.Tests.VBNet    --configuration %configuration% ^
-  && dotnet pack src\Aqua                 --configuration %configuration% --include-symbols --include-source --output "artifacts" ^
-  && dotnet pack src\Aqua.Newtonsoft.Json --configuration %configuration% --include-symbols --include-source --output "artifacts" ^
-  && dotnet pack src\Aqua.protobuf-net    --configuration %configuration% --include-symbols --include-source --output "artifacts"
+  && dotnet pack src\Aqua                 --configuration %configuration% ^
+  && dotnet pack src\Aqua.Newtonsoft.Json --configuration %configuration% ^
+  && dotnet pack src\Aqua.protobuf-net    --configuration %configuration%
