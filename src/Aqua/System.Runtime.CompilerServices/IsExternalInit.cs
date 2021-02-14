1e021aaced396e2e91dd3f520e3c5da0cdfc6cd4
Christof Senn <christof.senn@gmail.com>
2021-02-14 17:37:59 +0100

    updated tools packages

--- src/Aqua/System.Runtime.CompilerServices/IsExternalInit.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
-#if !NET5_0
-namespace System.Runtime.CompilerServices
-{
-    internal class IsExternalInit
-    {
-    }
-}
-#endif
