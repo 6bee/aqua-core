c7fade170c46a3473a0548485fb0cb8d3333fe56
Christof Senn <christof.senn@gmail.com>
2021-08-29 14:28:07 +0200

    updated dependencies

--- pack.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set configuration=Debug
-clean ^
-  && dotnet test test\Aqua.Tests          --configuration %configuration% ^
-  && dotnet test test\Aqua.Tests.VBNet    --configuration %configuration% ^
-  && dotnet pack src\Aqua                 --configuration %configuration% ^
-  && dotnet pack src\Aqua.Newtonsoft.Json --configuration %configuration% ^
-  && dotnet pack src\Aqua.protobuf-net    --configuration %configuration% ^
-  && dotnet pack src\Aqua.Text.Json       --configuration %configuration%
