eb144c2babe7be39e9fdaf7adfe50883db4bc41a
Christof Senn <christof.senn@gmail.com>
2021-09-03 14:00:30 +0200

    fixed null handling and nullable attribution

--- src/Aqua/Dynamic/DynamicObjectMapperExtensions.cs
+++ src/Aqua/Dynamic/DynamicObjectMapperExtensions.cs
@@ -34,10 +34,15 @@ namespace Aqua.Dynamic
         /// <param name="objects">Collection of <see cref="DynamicObject" /> to be mapped.</param>
         /// <param name="type">Target type for mapping, set this parameter to <see langword="null"/> if type information included within individual <see cref="DynamicObject" />s should be used.</param>
         /// <returns>Collection of objects created based on the <see cref="DynamicObject" />s specified.</returns>
-        public static IEnumerable Map(this IDynamicObjectMapper objectMapper, IEnumerable<DynamicObject?> objects, Type? type = null)
+        [return: NotNullIfNotNull("objects")]
+        public static IEnumerable? Map(this IDynamicObjectMapper objectMapper, IEnumerable<DynamicObject?>? objects, Type? type = null)
         {
             objectMapper.AssertNotNull(nameof(objectMapper));
-            objects.AssertNotNull(nameof(objects));
+
+            if (objects is null)
+            {
+                return default;
+            }
 
             IEnumerable<object?> source = objects.Select(x => objectMapper.Map(x, type));
             return type is null || type == typeof(object)
@@ -45,7 +50,6 @@ namespace Aqua.Dynamic
                 : source.CastCollectionToArrayOfType(type);
         }
 
-#nullable disable
         /// <summary>
         /// Maps a collection of <see cref="DynamicObject" />s into a collection of <typeparamref name="T" />.
         /// </summary>
@@ -53,9 +57,18 @@ namespace Aqua.Dynamic
         /// <param name="objectMapper">The <see cref="IDynamicObjectMapper"/> instance used to map the <see cref="DynamicObject"/>s.</param>
         /// <param name="objects">Collection of <see cref="DynamicObject" />s to be mapped.</param>
         /// <returns>Collection of <typeparamref name="T" /> created based on the <see cref="DynamicObject" />s specified.</returns>
-        public static IEnumerable<T> Map<T>(this IDynamicObjectMapper objectMapper, IEnumerable<DynamicObject> objects)
-            => (IEnumerable<T>)objectMapper.Map(objects, typeof(T));
-#nullable restore
+        [return: NotNullIfNotNull("objects")]
+        public static IEnumerable<T>? Map<T>(this IDynamicObjectMapper objectMapper, IEnumerable<DynamicObject>? objects)
+        {
+            objectMapper.AssertNotNull(nameof(objectMapper));
+
+            if (objects is null)
+            {
+                return default;
+            }
+
+            return (IEnumerable<T>)objectMapper.Map(objects, typeof(T));
+        }
 
         /// <summary>
         /// Maps a collection of objects into a collection of <see cref="DynamicObject"/>.
