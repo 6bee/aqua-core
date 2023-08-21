a11db09cc8b017dd0c73d07c5e8e989a3201647e
Christof Senn <christof.senn@gmail.com>
2023-08-21 21:36:33 +0200

    add ITypeResolver.TryResolveType(...) extension method

---
+++ src/Aqua/TypeSystem/TypeResolverExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+namespace Aqua.TypeSystem
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+
+    public static class TypeResolverExtensions
+    {
+        public static bool TryResolveType(this ITypeResolver typeResolver, TypeInfo type, [NotNullWhen(true)] out Type? resolvedType)
+        {
+            typeResolver.AssertNotNull();
+
+            try
+            {
+                resolvedType = typeResolver.ResolveType(type);
+                return true;
+            }
+            catch (TypeResolverException)
+            {
+                resolvedType = null;
+                return false;
+            }
+        }
+    }
+}
