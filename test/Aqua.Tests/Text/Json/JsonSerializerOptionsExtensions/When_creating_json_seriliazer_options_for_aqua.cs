8fb4098a2f2ac71ef04f979644144025c3027762
Christof Senn <christof.senn@option.ch>
2026-05-29 01:58:23 +0200

    add initial unit test for json serializer options extensions

---
+++ test/Aqua.Tests/Text/Json/JsonSerializerOptionsExtensions/When_creating_json_seriliazer_options_for_aqua.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Christof Senn. All rights reserved. See license.txt in the project root for license information.
+
+namespace Aqua.Tests.Text.Json.JsonSerializerOptionsExtensions;
+
+using System.Text.Json;
+
+public sealed class When_creating_json_seriliazer_options_for_aqua
+{
+    [Fact]
+    public void Should_set_reference_handler()
+    {
+        var defaultOptions = new JsonSerializerOptions();
+        defaultOptions.ReferenceHandler.ShouldBeNull();
+
+        var aquaOptions = defaultOptions.ConfigureAqua();
+        aquaOptions.ReferenceHandler.ShouldNotBeNull();
+    }
+}
